7d57295852eecc3b137a419a5538dc7b1081794f
Michael Green-Lab Computer <mjgreen2g@yahoo.com>
2022-01-07 18:58:03 -0600

    Edited formatting

--- prob_calculator.py
+++ prob_calculator.py
@@ -7,54 +7,57 @@ import random
 # Consider using the modules imported above.
 
 class Hat:
+
     def __init__(self, **balls):
         self.__dict__.update(balls)
-        colorList = list(self.__dict__.keys())
+        color_list = list(self.__dict__.keys())
         self.contents = []
 
         #convert dict (balls) into list (contents). Ex. {red:2} -> [red, red]
-        for ballCntr in range(0, len(colorList), 1) :
-            numberBall = self.__dict__.get(colorList[ballCntr], 0)
-            for colorCntr in range(0, numberBall, 1):
-                self.contents.append(colorList[ballCntr])
+        for ball_cntr in range(0, len(color_list), 1) :
+            number_ball = self.__dict__.get(color_list[ball_cntr], 0)
+            for color_cntr in range(0, number_ball, 1):
+                self.contents.append(color_list[ball_cntr])
 
     '''The `Hat` class should have a `draw` method that accepts an argument 
     indicating the number of balls to draw from the hat. This method should 
     remove balls at random from `contents` and return those balls as a list of strings. 
     The balls should not go back into the hat during the draw, 
-    similar to an urn experiment without replacement. If the number of balls to draw 
-    exceeds the available quantity, return all the balls.'''
-    def draw(self, drawNumber):
-        numberToDraw = drawNumber
-        if numberToDraw > len(self.contents):
-            numberToDraw = len(self.contents)
+    similar to an urn experiment without replacement. If the number of balls to draw exceeds the available quantity, return all the balls.
+    '''
+    def draw(self, draw_number):
+        number_to_draw = draw_number
+        if number_to_draw > len(self.contents):
+            number_to_draw = len(self.contents)
 
-        ballList = []
-        for drawCntr in range(0, numberToDraw, 1):
+        ball_list = []
+        for draw_cntr in range(0, number_to_draw, 1):
             ball = random.choice(self.contents)
             self.contents.remove(ball)
-            ballList.append(ball)
+            ball_list.append(ball)
 
-        return ballList
+        return ball_list
+
 
 '''The `experiment` function should return a probability. Perform `N` experiments, 
 count how many times `M` we get at least 'expected_balls', 
-and estimate the probability as `M/N`.'''
+and estimate the probability as `M/N`.
+'''
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):   
     successful_experiments = 0
-    for expCntr in range(0, num_experiments, 1):
+    for exp_cntr in range(0, num_experiments, 1):
         match = 0
         duplicate = copy.deepcopy(hat)
-        rawResults = duplicate.draw(num_balls_drawn)
+        raw_results = duplicate.draw(num_balls_drawn)
         #convert rawResults list into dict for comparison
-        processedResults = list_to_dict(rawResults)
+        processed_results = convert_list_to_dict(raw_results)
 
         #check if color is in hat. If yes, compare values. 
         # Match = number of drawn balls equals/exceeds number expected_balls 
-        expBallsList = list(processedResults.keys())
-        for checkCntr in range(0, len(expBallsList), 1):
-            if expBallsList[checkCntr] in expected_balls:
-                if processedResults.get(expBallsList[checkCntr]) >= expected_balls.get(expBallsList[checkCntr]):
+        balls_from_draw_list = list(processed_results.keys())
+        for check_cntr in range(0, len(balls_from_draw_list), 1):
+            if balls_from_draw_list[check_cntr] in expected_balls:
+                if processed_results.get(balls_from_draw_list[check_cntr]) >=   expected_balls.get(balls_from_draw_list[check_cntr]):
                     match += 1
                 else:
                     continue
@@ -66,21 +69,21 @@ def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
 
     return successful_experiments/num_experiments
 
-'''Function: list of colors to dict of color:number
-Ex. [red, green, red] -> {green:1, red:2}'''
-def list_to_dict(list):
+# Function: list of colors to dict of color:number
+# Ex. [red, green, red] -> {green:1, red:2}
+def convert_list_to_dict(list):
     conversion = {}
     list.sort()
     value = 0
-    listKey = list[0]
-    for lstCntr in range(0, len(list), 1):
-        item = list[lstCntr]
-        if item is listKey :
+    list_key = list[0]
+    for list_cntr in range(0, len(list), 1):
+        item = list[list_cntr]
+        if item is list_key :
             value += 1
         else:
-            conversion[listKey] = value
-            listKey = item
+            conversion[list_key] = value
+            list_key = item
             value = 1
 
-    conversion[listKey] = value
+    conversion[list_key] = value
     return conversion
